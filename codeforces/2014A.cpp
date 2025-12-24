6138442385f0e0aea1b846c23dcbf8acc486605b
Andrew <104359161+Andrew7441@users.noreply.github.com>
2025-12-24 17:04:24 +0200

    Add problem

---
+++ codeforces/2014A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> a(n+1);
+        for(int i = 1; i <= n; i++) cin >> a[i];
+
+        int rb = 0;
+        int res = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(a[i] >= k){
+                rb = a[i];
+            }
+            if(a[i] == 0 && rb > 0){
+                rb--;
+                a[i]++;
+                res++;
+            }
+        }
+
+        cout << res << "\n";
+
+    }
+
+    return 0;
+}
---
+++ codeforces/2161A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int r, X, D, N;
+        cin >> r >> X >> D >> N;
+
+        
+    }
+
+    return 0;
+}
