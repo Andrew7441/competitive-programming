4927723c52ab35b36b5775420d5e24a44b16d3c5
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-01-05 12:33:03 +0200

    Add Fizz Buzz Multithreaded

---
+++ leetcode/1195.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class FizzBuzz {
+private:
+    int n;
+    int i = 1;
+    mutex m;
+    condition_variable cv;
+
+public:
+    FizzBuzz(int n) {
+        this->n = n;
+    }
+
+    // printFizz() outputs "fizz".
+    void fizz(function<void()> printFizz) {
+        while(true){
+            unique_lock<mutex> lock(m);
+            cv.wait(lock, [&](){
+                return i > n || (i % 3 == 0 && i % 5 != 0);
+            });
+
+            if(i > n) return;
+
+            printFizz();
+
+            i++;
+            cv.notify_all();
+        }
+    }
+
+    // printBuzz() outputs "buzz".
+    void buzz(function<void()> printBuzz) {
+        while(true){
+            unique_lock<mutex> lock(m);
+            cv.wait(lock, [&](){
+                return i > n || (i % 5 == 0 && i % 3 != 0);
+            });
+
+            if(i > n) return;
+
+            printBuzz();
+
+            i++;
+            cv.notify_all();
+
+        }
+    }
+
+    // printFizzBuzz() outputs "fizzbuzz".
+	void fizzbuzz(function<void()> printFizzBuzz) {
+        while(true){
+            unique_lock<mutex> lock(m);
+            cv.wait(lock, [&](){
+                return i > n || (i % 3 == 0 && i % 5 == 0);
+            });
+
+            if(i > n) return;
+            
+            printFizzBuzz();
+            
+            i++;
+            cv.notify_all();
+        }
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void number(function<void(int)> printNumber) {
+        while(true){
+            unique_lock<mutex> lock(m);
+            cv.wait(lock, [&](){
+                return i > n || (i % 3 != 0 && i % 5 != 0);
+            });
+
+            if(i > n) return;
+
+            printNumber(i);
+
+            i++;
+            cv.notify_all();
+        }
+    }
+};
+
+int main() {
+    FizzBuzz Sol(15);
+    
+    
+}
