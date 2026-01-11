6497abbbf126c2910cf07de95789e7ccf3ffb924
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-01-11 16:49:37 +0200

    Check Distances Between Same Letters

---
+++ leetcode/2399.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool checkDistances(string s, vector<int>& distance) {
+        vector<int> firstpos(26, -1);
+
+        for(int i = 0; i < s.size(); i++){
+            int idx = s[i] - 'a';
+            if(firstpos[idx] == -1){
+                firstpos[idx] = i;
+            }else{
+                if(i - firstpos[idx] - 1 != distance[idx]){
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+};
+
+int main() {
+    cout << boolalpha;
+    Solution Sol;
+    vector<int> distance{1,3,0,5,0,0,0,0,0,0,0,0,
+                        0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+
+    cout << Sol.checkDistances("abaccb", distance);
+
+    
+}
