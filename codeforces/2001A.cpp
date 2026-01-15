1757510709d24b63ba9e7f706ac72758895ea3c8
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-01-15 14:17:32 +0200

    Make All Equal

---
+++ codeforces/2001A.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(NULL);
+
+  int t; cin >> t;
+  while(t--) {
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int &x : a) cin >> x;
+
+    vector<int> freq(n + 1);
+    for(int x : a) freq[x]++;
+
+    cout << n - (*max_element(freq.begin(), freq.end())) << '\n';
+  }
+}
