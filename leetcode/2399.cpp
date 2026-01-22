8cbb81089ab0a242c869bb4a28f4cb63b8953e4e
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-01-22 16:58:19 +0200

    Add 2399.cpp

--- leetcode/2399.cpp
+++ leetcode/2399.cpp
@@ -6,7 +6,7 @@ public:
     bool checkDistances(string s, vector<int>& distance) {
         vector<int> firstpos(26, -1);
 
-        for(int i = 0; i < s.size(); i++){
+        for(int i = 0; i < (int)s.size(); i++){
             int idx = s[i] - 'a';
             if(firstpos[idx] == -1){
                 firstpos[idx] = i;
