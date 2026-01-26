cfc90a11b1214882b42b81e200b0bf395bdfc089
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-01-26 13:08:49 +0200

    Add Sleeping Through Classes

--- codeforces/2173A.cpp
+++ codeforces/2173A.cpp
@@ -11,7 +11,7 @@ void solve(){
     int x = 0;
     int res = 0;
 
-    for(int i = 0; i < s.length(); i++){
+    for(int i = 0; i < n; i++){
         if(s[i] == '1')
             x = k;
         else if(s[i] == '0' && x > 0){
