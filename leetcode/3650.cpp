1a4ba9e1b220938e07c4db6a73a7ba4ef4743fb8
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-01-27 14:33:08 +0200

    Add Minimum Cost Path with Edge Reversals

---
+++ leetcode/3650.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minCost(int n, vector<vector<int>>& edges) {
+        vector<vector<pair<int, int>>> adj(n);
+        vector<int> dist(n, INT_MAX);
+        for(auto it : edges){
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], 2 * it[2]});
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        dist[0] = 0;
+        pq.push({0,0});
+
+        while(!pq.empty()){
+            int distance = pq.top().first;
+            int node = pq.top().second;
+
+            pq.pop();
+            if(distance > dist[node])
+                continue;
+            
+            if(node == n - 1)
+                return distance;
+            
+            for(auto it : adj[node]){
+                int adjnode = it.first;
+                int weight = it.second;
+                if(distance + weight < dist[adjnode]){
+                    dist[adjnode] = distance + weight;
+                    pq.push({distance + weight, adjnode});
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+
+int main() {
+    vector<vector<int>> edges{{0,2,1},{2,1,1},{1,3,1},{2,3,3}};
+
+    Solution S;
+
+    cout << S.minCost(4, edges);
+}
