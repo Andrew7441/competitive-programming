6e05892e60ad3941984c930c69f9b20f856511dd
Andrew7441 <andrewqumsieh12345@gmail.com>
2026-02-03 21:15:15 +0200

    Add Trionic Array I

---
+++ leetcode/3637.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size(), i = 0;
+
+        if(n < 4) return false;    
+
+        // strictly increasing 
+        while(i + 1 < n && nums[i] < nums[i+1]) i++;
+        if(i == 0) return false;
+        int p = i;
+
+        // strictly decreasing
+        while(i + 1 < n && nums[i] > nums[i+1]) i++;
+        if(i == p) return false;
+        int q = i;
+
+        //strictly increasing
+        while(i + 1 < n && nums[i] < nums[i+1]) i++;
+        
+
+        return i == n - 1 && q < n - 1;
+    }
+};
