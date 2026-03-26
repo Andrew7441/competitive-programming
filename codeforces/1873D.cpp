ba452c02808c46dd9729780f467ac576748a4788
Andrew7441 <andrewqumsieh12345@gmail.com>
2026-03-26 11:34:27 +0200

    1D Eraser

---
+++ codeforces/1873D.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+
+    string s;
+    cin >> s;
+
+    int res = 0;
+    int i = 0;
+    while(i < n){
+        if(s[i] == 'B'){
+            res++;
+            i+=k;
+        }else{
+            i++;
+        }
+    
+    }
+
+    cout << res << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
