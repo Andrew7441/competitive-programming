b175c6e68dd80108d4bef341038c1d0a9144641d
Andrew7441 <andrewqumsieh12345@gmail.com>
2026-04-14 17:53:02 +0300

    Add problems

---
+++ codeforces/2147B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**/
+
+void solve(){
+    int n;
+    cin >> n;
+
+    for(int i = n; i >= 1; i--){
+        cout << i << " ";
+    }
+
+    cout << n << " "; 
+
+    for(int i = 1; i < n; i++){
+        cout << i << " ";
+    }
+
+    cout << "\n";
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
